b2a7f8e5c2ac1813a5a4d20713e06ca877cf2e16
Víctor <victor.af1104@gmail.com>
2024-07-14 22:30:07 +0200

    Update portfolio using midudev theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), robotsTxt()],
+  site: 'https://victoralban.dev/',
 });
---
+++ src/constants/constants.ts
@@ -0,0 +1,96 @@
+import AngularIcon from '@/components/icons/Angular.astro';
+import AstroIcon from '@/components/icons/BrandAstro.astro';
+import AWSIcon from '@/components/icons/AWS.astro';
+import CSSIcon from '@/components/icons/CSS3.astro';
+import HTMLIcon from '@/components/icons/HTML.astro';
+import JavaScriptIcon from '@/components/icons/JavaScript.astro';
+import NestJSIcon from '@/components/icons/NestJS.astro';
+import NextJSIcon from '@/components/icons/NextJS.astro';
+import PHPIcon from '@/components/icons/PHP.astro';
+import PythonIcon from '@/components/icons/Python.astro';
+import ReactJSIcon from '@/components/icons/React.astro';
+import SASSIcon from '@/components/icons/SASS.astro';
+import TailwindIcon from '@/components/icons/Tailwind.astro';
+import TypeScriptIcon from '@/components/icons/TypeScript.astro';
+import WordPressIcon from '@/components/icons/WordPress.astro';
+
+export const TAGS = {
+  ANGULAR: {
+    name: 'Angular',
+    class: 'bg-[#dd0031] text-white',
+    icon: AngularIcon,
+  },
+  ASTRO: {
+    name: 'Astro',
+    class: 'bg-[#23262F] text-[#E03775]',
+    classText: 'text-white',
+    icon: AstroIcon,
+  },
+  AWS: {
+    name: 'AWS',
+    class: 'bg-[#232f3e] text-white',
+    icon: AWSIcon,
+  },
+  CSS: {
+    name: 'CSS',
+    class: 'bg-[#1572b6] text-white',
+    icon: CSSIcon,
+  },
+  HTML: {
+    name: 'HTML',
+    class: 'bg-[#e34f26] text-white',
+    icon: HTMLIcon,
+  },
+  JAVASCRIPT: {
+    name: 'JavaScript',
+    class: 'bg-[#f7df1e] text-[#080808]',
+    classText: 'text-black',
+    icon: JavaScriptIcon,
+  },
+  NESTJS: {
+    name: 'NestJS',
+    class: 'bg-[#0E0E10] text-white',
+    icon: NestJSIcon,
+  },
+  NEXTJS: {
+    name: 'Next.js',
+    class: 'bg-black text-white',
+    icon: NextJSIcon,
+  },
+  PHP: {
+    name: 'PHP',
+    class: 'bg-[#8892be] text-white',
+    icon: PHPIcon,
+  },
+  PYTHON: {
+    name: 'Python',
+    class: 'bg-[#306998] text-white',
+    icon: PythonIcon,
+  },
+  REACTJS: {
+    name: 'React',
+    class: 'bg-[#222730] text-[#57C4DC]',
+    classText: 'text-white',
+    icon: ReactJSIcon,
+  },
+  SCSS: {
+    name: 'SCSS',
+    class: 'bg-[#BF4080] text-white',
+    icon: SASSIcon,
+  },
+  TAILWIND: {
+    name: 'Tailwind CSS',
+    class: 'bg-[#003159] text-white',
+    icon: TailwindIcon,
+  },
+  TYPESCRIPT: {
+    name: 'TypeScript',
+    class: 'bg-[#3178c6] text-white',
+    icon: TypeScriptIcon,
+  },
+  WORDPRESS: {
+    name: 'WordPress',
+    class: 'bg-[#21759b] text-white',
+    icon: WordPressIcon,
+  },
+};
