06bddbd2bfc9cfd99befdfbccb14a00a4623e1ab
Víctor <victor.af1104@gmail.com>
2024-07-17 18:48:15 +0200

    Remove astro-robots-txt integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [tailwind()],
   site: 'https://victoralban.dev/',
 });
