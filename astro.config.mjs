0163accf87a37fbd36dde98223825136d144f6b9
Víctor <victor.af1104@gmail.com>
2024-07-17 18:55:14 +0200

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://victoralban.dev/',
+  integrations: [tailwind(), sitemap()],
+  site: 'https://victoralban.dev/'
 });
