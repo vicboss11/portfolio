4b5d557e1ecb3518fa1e5f6618d62ebc233b2910
Víctor <victor.af1104@gmail.com>
2025-06-04 17:02:36 +0200

    Add Partytown integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), partytown()],
   site: 'https://victoralban.dev/'
 });
